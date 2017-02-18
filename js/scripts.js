a97d02cbc0507fb55417bb26505f6a35ce79c81c
bookwormgirl910 <whykatherine@gmail.com>
2017-02-18 15:59:00 +0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	var getInfo = function() {
+		var entry = $(".entry").val();
+		console.log(entry)
+		if(entry == "") {
+			$(".info").prepend("<p class='loading'>Please enter a word.</p>")
+		} else {
+			$.ajax({
+				type: "GET",
+				url: "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + entry + "?key=387115b8-0a9e-464f-8f56-1e4a6f46f7f1",
+				crossDomain: true,
+				datatype: "xml"
+			});
+		}
+	}
+
+	$(".search-btn").click(getInfo());
+	$(".entry").keyup(function(event) {
+		if(event.keyCode == 13) {
+			getInfo();
+		}
+	});
+});
+
