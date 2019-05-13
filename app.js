10530631f139e4bf20b7ae2814e8800fbe9041ec
whykatherine <whykatherine@gmail.com>
2019-05-13 17:26:55 -0400

    feat: display sample entry

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,22 +14,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
@@ -42,5 +39,20 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// app.get('/', function(req, res) {
+//   res.render('index', {
+//   title: 'Etymologee',
+//   entry: indexRouter.entry
+//   });
+// });
+
+// app.get('/input_word', function(req, res) {
+//   var inp = req.query.word;
+//   res.send('Word: ' + inp);
+// });
+// app.post('/input_word', function(req, res) {
+//   res.send('Word: ' + req.body.word);
+// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,70 @@
 var express = require('express');
 var router = express.Router();
 
+var https = require('https');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+var concat = require('concat-stream');
+var util = require('util');
+
+var word = 'ketchup';
+var entry = {"word":"", "etymology":"", "date":null, "definition":""};
+
+parser.on('error', function(err) {
+  console.log('Parser error', err);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var url = 'https://www.dictionaryapi.com/api/v1/references/collegiate/xml/' +
+  word + '?key=387115b8-0a9e-464f-8f56-1e4a6f46f7f1';
+
+  https.get(url, function(resp) {
+
+    resp.on('error', function(err) {
+      console.log('Error while reading', err);
+    });
+
+    resp.pipe(
+      concat(function(buffer) {
+        var str = buffer.toString();
+        parser.parseString(str, function(err, result) {
+          var entry_list = result.entry_list;
+          var entries = entry_list.entry;
+          var first_entry = entries[0];
+          var ew = first_entry.ew[0];
+          // console.log('Word: ' + word);
+          var et = first_entry.et[0];
+          // console.log('Etymology: ' + util.inspect(et));
+          var def = first_entry.def[0];
+          var date = def.date[0];
+          // console.log('Date: ' + date);
+          var dt = def.dt[0];
+          // console.log('Definition: ' + util.inspect(dt));
+
+          entry["word"] = ew;
+          entry["etymology"] = et;
+          entry["date"] = date;
+          entry["definition"] = dt;
+        });
+      })
+    );
+
+    // console.log(entry);
+
+  });
+
+  res.render('index', {
+    title: 'Etymologee',
+    entry: entry
+  });
+
+});
+
+router.post('/input', function(req, res, next) {
+  word = req.body.word;
+  res.redirect('/');
 });
 
 module.exports = router;
