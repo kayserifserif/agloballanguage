9f859408bdbd5058fd61b4b11fe97038aae3c560
whykatherine <whykatherine@gmail.com>
2019-12-10 23:20:15 -0800

    fix: collect all origins instead of just one entry

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,11 +1,15 @@
 window.onload = function() {
   var container = document.getElementById("container");
   var globe = new DAT.Globe(container);
-  var origins = null;
-  var entry_num = 0;
-  while (!origins && entry_num < my_entries.length) {
-    origins = my_entries[entry_num].origins;
-    entry_num++;
+  var origins = {};
+  for (var entry_num = 0; entry_num < my_entries.length; entry_num++) {
+    var entry_origins = my_entries[entry_num].origins;
+    for (language in entry_origins) {
+      origins[language] = {
+        "lat": entry_origins[language].lat,
+        "lon": entry_origins[language].lon
+      };
+    }
   }
   console.log(my_entries[0].word, origins);
   if (origins && Object.keys(origins).length > 0) {
